2b9f905d0f5c5e07c55714fa57cc346b38224670
matias <matiaskochman@gmail.com>
2021-06-30 09:28:08 -0300

    bugfix

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -20,24 +20,24 @@ contract TokenFarm {
 
   // 1 stakes tokens (deposit)
 
-  // function stake(uint _amount) public {
-  //   // Transfer mock dai tokens to this contract for staking
-  //   // dai tokens are being transfered from the sender account to the tokenFarm account in the 
-  //   // mockdai contract.
-  //   daiToken.transferFrom(msg.sender, address(this), _amount);
+  function stake(uint _amount) public {
+    // Transfer mock dai tokens to this contract for staking
+    // dai tokens are being transfered from the sender account to the tokenFarm account in the 
+    // mockdai contract.
+    daiToken.transferFrom(msg.sender, address(this), _amount);
 
-  //   // Update staking balance
-  //   stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+    // Update staking balance
+    stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
-  //   // Add user to stakers array only if they haven't staked already.
-  //   if(!hasStaked(msg.sender)){
-  //     stakers.push(msg.sender);
-  //   }
+    // Add user to stakers array only if they haven't staked already.
+    if(!hasStaked[msg.sender]){
+      stakers.push(msg.sender);
+    }
 
-  //   // Update staking status
-  //   hasStaked[msg.sender] = true;
-  //   isStaking[msg.sender] = true;
-  // }
+    // Update staking status
+    hasStaked[msg.sender] = true;
+    isStaking[msg.sender] = true;
+  }
   // 2 unstake tokens (withdraw)
 
   // 3 issue tokens
